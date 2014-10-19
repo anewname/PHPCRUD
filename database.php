47b3b931a5ec275ad0ad7e1c61ff72ae595e9e80
jtianwen <jtianwen2013@gmail.com>
2014-10-19 20:29:10 +0800

    last version

--- database.php
+++ database.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 class Database {
 	private static $dbName = 'bookdb';
 	private static $dbHost = 'localhost';
@@ -26,43 +25,5 @@ class Database {
 		self::$cont = null;
 	}
 }
-=======
-	class Database
-	{
-		private static $dbName = 'address_book';
-		private static $dbHost = 'localhost';
-		private static $dbUsername = 'root';
-		private static $dbUserPassword = 'apple610';
-		
-		private static $cont = null;
-		
-		public function __construct() 
-		{
-      	die('Init function is not allowed');
-    	}
-		public static function connect()
-    	{
-       // One connection through whole application
-      	if ( null == self::$cont )
-       	{     
-        		try
-        		{
-          		self::$cont =  new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserPassword); 
-        		}
-        		catch(PDOException $e)
-        		{
-          		die($e->getMessage()); 
-        		}
-       	}
-       	return self::$cont;
-    	}
-     
-    	public static function disconnect()
-    	{
-        	self::$cont = null;
-    	}
-
-	}
->>>>>>> a711c95126ba14ca06504d34e4dd8e02b884afd1
 ?>
 
